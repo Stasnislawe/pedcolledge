c063e8ec8ece6b7d8a15c849f290c6ec77b7c445
stas_ <dromanqs@mail.ru>
2025-05-24 22:18:59 +0500

    Добавление моделей в админ-панель (Users, TestResults, UserAnswers, Quizzes, Questions, Options)

--- core/web/admin.py
+++ core/web/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Patient
+from .models import Users, TestResults, UserAnswers, Quizzes, Questions, Options
 
 
-admin.site.register(Patient)
+admin.site.register(Users)
+admin.site.register(TestResults)
+admin.site.register(UserAnswers)
+admin.site.register(Quizzes)
+admin.site.register(Questions)
+admin.site.register(Options)
